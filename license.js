6c716e61946b73c1853483c749fa9bcd9c6f0964
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-08-03 14:27:31 +0300

    Update license.js

--- license.js
+++ license.js
@@ -1,1 +1,9 @@
 
+// license.js
+const OWNER_NUMBER = "+255719632816"; // Namba yako tu
+
+function isLicensed(userNumber) {
+  return userNumber === OWNER_NUMBER;
+}
+
+module.exports = { isLicensed };
