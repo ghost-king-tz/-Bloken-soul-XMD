f778c23417858fbcad2cd69c17925419f9f108b8
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-08-24 03:40:43 +0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ownerNumber: "255719632816", // 👈 Hapa weka namba yako bila "+"
+    ownerName: "𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝",
+    sessionPath: "./session", // path ya kuhifadhi session
+    botName: "Broken Soul-XMD",
+    prefix: ".", // command prefix (mfano: .menu, .help)
+};
