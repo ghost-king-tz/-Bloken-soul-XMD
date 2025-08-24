7aa8d3203b48fbf3be770789dd52e8507de016ba
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-08-24 16:19:39 +0300

    Delete command directory
    It's wrong i will create it again soon 😭😭

--- command/ping.js
+++
@@ -1,7 +0,0 @@
-const handler = async (m, { conn }) => {
-  await m.reply("🟢 Bot is online and working!");
-};
-handler.help = ['ping'];
-handler.tags = ['info'];
-handler.command = /^ping$/i;
-export default handler;
