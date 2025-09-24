b2cd45717ade11d0ad1d7c63238962e0ba438d84
𝐆𝐇𝐎𝐒𝐓 𝐊𝐈𝐍𝐆 <hi2879639@gmail.com>
2025-09-24 11:46:27 +0300

    Delete settings.js

--- settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    ownerNumber: "255613420746", // 👈 Hapa weka namba yako bila "+"
-    ownerName: "☬Black Night☬",
-    sessionPath: "./session", // path ya kuhifadhi session
-    botName: "Broken Soul-XMD",
-    prefix: ".", // command prefix (mfano: .menu, .help)
-};
