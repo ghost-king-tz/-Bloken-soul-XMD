e9dfdcb7880590fef3da7f2c43b2ded7b29ca65c
𝐆𝐇𝐎𝐒𝐓 𝐊𝐈𝐍𝐆 <hi2879639@gmail.com>
2025-09-24 21:41:41 +0300

    Update ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,8 +4,22 @@ import { calculatePing } from '../utils/helper.utils'
 export default {
     category: 'general',
     description: `To find out the response from the bot`,
-    callback: async ({ msg, User, args, message, client }) => {
-        console.log(User)
-        msg.reply(`*_${calculatePing(msg.messageTimestamp, Date.now())} second(s)_*`)
+    callback: async ({ msg, client }) => {
+        const ping = calculatePing(msg.messageTimestamp, Date.now())
+
+        // Reply ping time
+        await msg.reply(`*_${ping} second(s)_*`)
+
+        // Send image
+        await client.sendMessage(msg.from, {
+            image: { url: 'https://files.catbox.moe/la2zx7.jpg' },
+            caption: `Ping result: ${ping} second(s)`
+        })
+
+        // Send audio
+        await client.sendMessage(msg.from, {
+            audio: { url: 'https://files.catbox.moe/myx011.mp3' },
+            mimetype: 'audio/mp4'
+        })
     },
 } as ICommand
