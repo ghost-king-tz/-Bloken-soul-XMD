894da6665c0c598e4c682e49147eab843d8698ff
𝐆𝐇𝐎𝐒𝐓 𝐊𝐈𝐍𝐆 <hi2879639@gmail.com>
2025-09-24 11:47:22 +0300

    Delete license.js

--- license.js
+++
@@ -1,9 +0,0 @@
-
-// license.js
-const OWNER_NUMBER = "+255719632816"; // Namba yako tu
-
-function isLicensed(userNumber) {
-  return userNumber === OWNER_NUMBER;
-}
-
-module.exports = { isLicensed };
