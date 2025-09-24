15fda8d4ac3957b73e6fd0b79751e8ef23888ae7
𝐆𝐇𝐎𝐒𝐓 𝐊𝐈𝐍𝐆 <hi2879639@gmail.com>
2025-09-24 14:29:38 +0300

    Add files via upload

---
+++ src/commands/random/weebao/cringe.ts
@@ -0,0 +1,14 @@
+import { ICommand } from '@constants'
+import { getBuffer } from '@utils/helper.utils'
+import { lolhuman, footer } from '@config'
+
+export default {
+    description: 'cringe random',
+    category: 'weeaboo-random',
+    consume: 3,
+    callback: async ({ client, msg, prefix }) => {
+        const { from, sender } = msg
+        let __buff = await getBuffer(`https://api.lolhuman.xyz/api/random/cringe?apikey=${lolhuman}`)
+        return client.sendMessage(from, { image: __buff, jpegThumbnail: __buff, mentions: [sender], gifPlayback: true })
+    },
+} as ICommand
